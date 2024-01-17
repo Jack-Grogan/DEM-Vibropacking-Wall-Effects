e6e2b2d3c21af103683b5f9f60dcedf412c815e0
Jack Grogan <142009601+Jack-Grogan@users.noreply.github.com>
2024-01-17 12:33:43 +0000

    Add files via upload

---
+++ method_2_wall_extremes/final_voxel_packing_method_sliding.py
@@ -0,0 +1,151 @@
+"""
+@author: Jack Richard Grogan
+
+      ___  ________  ________  ___  __            ________
+     |\  \|\   __  \|\   ____\|\  \|\  \         |\   ____\
+     \ \  \ \  \|\  \ \  \___|\ \  \/  /|_       \ \  \___|
+   __ \ \  \ \   __  \ \  \    \ \   ___  \       \ \  \  ___
+  |\  \\_\  \ \  \ \  \ \  \____\ \  \\ \  \       \ \  \|\  \
+  \ \________\ \__\ \__\ \_______\ \__\\ \__\       \ \_______\
+   \|________|\|__|\|__|\|_______|\|__| \|__|        \|_______|
+
+"""
+
+import numpy as np
+import pyvista as pv
+import os
+import glob
+from natsort import natsorted
+import pandas as pd
+import konigcell as kc
+
+# relative path to particle-wall sliding friction study
+
+study = os.path.join("10_mm_diameter_particles", "sliding_pw")
+
+# list extreme particle-particle interactions to investigate
+
+glob_input_study = os.path.join(study, "sliding_*")
+study = natsorted([k for k in glob.glob(glob_input_study)])
+
+# setting up list of final packing density values
+
+voxel_density_bank = []
+
+# setting up list of column names
+
+columns = []
+
+for study_directory in study:
+
+    # setting up seed values to run through
+
+    glob_input_seeds = os.path.join(study_directory, "seed_*")
+    seeds = natsorted([k for k in glob.glob(glob_input_seeds)])
+
+    for seed in seeds:
+            
+        # assigning column names
+
+        column_name_part_1 = os.path.basename(os.path.normpath(study_directory)) 
+        column_name_part_2 = os.path.basename(os.path.normpath(seed))
+        my_separator = '_'
+        column_name = my_separator.join([column_name_part_1, column_name_part_2])
+
+        columns.append(column_name)
+
+        glob_input_directories = os.path.join(seed, "sliding_*")
+        directories = natsorted([k for k in glob.glob(glob_input_directories)])
+
+        # assigning row names
+
+        rows = []
+        voxel_density_list = []
+
+        for directory in directories:
+
+            row_data = os.path.basename(os.path.normpath(directory))
+            _, row_value = row_data.split("_")
+            rows.append(float(row_value))
+
+            # the final packing arrangement file
+
+            glob_input = os.path.join(directory, "post", "particles_*")
+            files = natsorted([f for f in glob.glob(glob_input) if "boundingBox" not in f])
+            end_file = files[-1]
+
+            cylinder_glob_input = os.path.join(directory, "post", "mesh_*")
+            cylinder_files = natsorted([f for f in glob.glob(cylinder_glob_input)])
+            cylinder_end_file = cylinder_files[-1]
+
+            print(end_file)
+            data = pv.read(end_file)
+
+            print(cylinder_end_file)
+            cylinder_data = pv.read(cylinder_end_file)
+            
+
+            # setting domain limits of Konigcell
+            
+            z_low = min(data.points[:,2]) + 0.25*(max(data.points[:,2]) - min(data.points[:,2]))
+            z_up = min(data.points[:,2]) + 0.75*(max(data.points[:,2]) - min(data.points[:,2]))
+        
+            x_low = cylinder_data.bounds[0]
+            x_up = cylinder_data.bounds[1]
+
+            y_low = cylinder_data.bounds[2]
+            y_up = cylinder_data.bounds[3]
+
+            z_lim = [z_low, z_up]
+            y_lim = [y_low, y_up]
+            x_lim = [x_low, x_up]
+
+            # Setting voxel resolution in x y and z
+        
+            res_x = 100
+            res_y = 100
+            res_z = 100
+        
+            # Voxelising data
+        
+            voxels = kc.static3d(data.points,
+                                kc.INTERSECTION,
+                                radii = data["radius"],
+                                resolution = (res_x,  res_y, res_z),
+                                xlim = x_lim,
+                                ylim = y_lim,
+                                zlim = z_lim,
+                                max_workers = 1,
+                                )
+            
+            voxel_region = (x_up - x_low)*(y_up - y_low)
+            cylinder_region = np.pi*((x_up - x_low)**2)/4
+            cylinder_fraction = cylinder_region/voxel_region
+
+            print(f"cylinder region: {cylinder_region}\n")
+            print(f"voxel region: {voxel_region}\n")
+            print(f"cylinder fraction: {cylinder_fraction}\n")
+
+        
+            voxels.voxels[:] /= np.prod(voxels.voxel_size)
+
+            pixels = kc.Pixels(
+                np.mean(voxels.voxels, axis=1),
+                xlim = voxels.xlim,
+                ylim = voxels.zlim,
+            )
+                
+            ypoints = np.mean(pixels.pixels, axis = 0)/cylinder_fraction
+        
+            # determining packing density between z_min and z_max
+        
+            voxels_packing_density = np.mean(ypoints)
+            voxel_density_list.append(voxels_packing_density)
+            
+        voxel_density_bank.append(voxel_density_list)
+voxel_density_bank = np.asarray(voxel_density_bank).T
+
+# writing data to csv file
+
+df = pd.DataFrame(voxel_density_bank, columns = columns, index = rows)
+df.to_csv('final_voxel_packing_sliding_results.csv', index = True)
---
+++ method_2_wall_extremes/sliding_packing_graphs.py
@@ -0,0 +1,201 @@
+"""
+@author: Jack Richard Grogan
+
+      ___  ________  ________  ___  __            ________     
+     |\  \|\   __  \|\   ____\|\  \|\  \         |\   ____\    
+     \ \  \ \  \|\  \ \  \___|\ \  \/  /|_       \ \  \___|    
+   __ \ \  \ \   __  \ \  \    \ \   ___  \       \ \  \  ___  
+  |\  \\_\  \ \  \ \  \ \  \____\ \  \\ \  \       \ \  \|\  \ 
+  \ \________\ \__\ \__\ \_______\ \__\\ \__\       \ \_______\
+   \|________|\|__|\|__|\|_______|\|__| \|__|        \|_______|
+   
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerTuple
+
+# Setting out graph colours
+
+colors = ['k','b',  'limegreen', '#FF0000']
+
+# Reading in data
+
+df = pd.read_csv(r"final_paper_packing_results_sliding.csv")
+df.rename({'Unnamed: 0': 'sliding_pw'}, axis=1, inplace=True)
+df_sliding_pw = np.asarray(df['sliding_pw'])
+
+s0_r0_columns = [column for column in df.columns if column.startswith("sliding_0_rolling_0")]
+s1_r0_columns = [column for column in df.columns if column.startswith("sliding_1_rolling_0")]
+s0_r1_columns = [column for column in df.columns if column.startswith("sliding_0_rolling_1")]
+s1_r1_columns = [column for column in df.columns if column.startswith("sliding_1_rolling_1")]
+
+df_s0_r0= df[s0_r0_columns].T
+df_s1_r0= df[s1_r0_columns].T
+df_s0_r1= df[s0_r1_columns].T
+df_s1_r1= df[s1_r1_columns].T
+
+# Converting data into arrays
+
+repeats_s0_r0 = [0]*len(df_sliding_pw)
+repeats_s1_r0 = [0]*len(df_sliding_pw)
+repeats_s0_r1 = [0]*len(df_sliding_pw)
+repeats_s1_r1 = [0]*len(df_sliding_pw)
+
+for i in range(len(df_sliding_pw)):
+    repeats_s0_r0[i] = np.asarray(df_s0_r0[i])
+    repeats_s1_r0[i] = np.asarray(df_s1_r0[i])
+    repeats_s0_r1[i] = np.asarray(df_s0_r1[i])
+    repeats_s1_r1[i] = np.asarray(df_s1_r1[i])
+
+data_s0_r0 = [0]*len(repeats_s0_r0[0])
+data_s1_r0 = [0]*len(repeats_s1_r0[0])
+data_s0_r1 = [0]*len(repeats_s0_r1[0])
+data_s1_r1 = [0]*len(repeats_s1_r1[0])
+
+for i in range(len(repeats_s0_r0[0])):
+    data_s0_r0[i] = np.asarray(df[s0_r0_columns[i]])
+    data_s1_r0[i] = np.asarray(df[s1_r0_columns[i]])
+    data_s0_r1[i] = np.asarray(df[s0_r1_columns[i]])
+    data_s1_r1[i] = np.asarray(df[s1_r1_columns[i]])
+
+# Creating figure
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 7), layout = 'constrained')
+
+# Creating scatterplot
+
+scatter_s0_r0           = []
+scatter_s0_r0_x_points  = []
+
+scatter_s1_r0           = []
+scatter_s1_r0_x_points  = []
+
+scatter_s0_r1           = []
+scatter_s0_r1_x_points  = []
+
+scatter_s1_r1           = []
+scatter_s1_r1_x_points  = []
+
+for i in data_s0_r0:
+    for j in i:
+        scatter_s0_r0.append(j)
+        
+for i in range(len(data_s0_r0)):
+    for j in df_sliding_pw:
+        scatter_s0_r0_x_points.append(j)
+
+for i in data_s1_r0:
+    for j in i:
+        scatter_s1_r0.append(j)
+        
+for i in range(len(data_s1_r0)):
+    for j in df_sliding_pw:
+        scatter_s1_r0_x_points.append(j)
+        
+for i in data_s0_r1:
+    for j in i:
+        scatter_s0_r1.append(j)  
+        
+for i in range(len(data_s0_r1)):
+    for j in df_sliding_pw:
+        scatter_s0_r1_x_points.append(j)
+                
+for i in data_s1_r1:
+    for j in i:
+        scatter_s1_r1.append(j)
+        
+for i in range(len(data_s1_r1)):
+    for j in df_sliding_pw:
+        scatter_s1_r1_x_points.append(j)
+    
+    
+s1 = plt.scatter(scatter_s0_r0_x_points, scatter_s0_r0, color = colors[0], alpha = 0.4, marker = 'x')#, label = "Particle-Particle SLiding Friction = 0 \nParticle-Particle Rolling Friction = 0 ")
+s2 = plt.scatter(scatter_s1_r0_x_points, scatter_s1_r0, color = colors[1], alpha = 0.4, marker = 'x')#, label = "Particle-Particle SLiding Friction = 1 \nParticle-Particle Rolling Friction = 0 ")
+s3 = plt.scatter(scatter_s0_r1_x_points, scatter_s0_r1, color = colors[2], alpha = 0.4, marker = 'x')#, label = "Particle-Particle SLiding Friction = 0 \nParticle-Particle Rolling Friction = 1 ")
+s4 = plt.scatter(scatter_s1_r1_x_points, scatter_s1_r1, color = colors[3], alpha = 0.4, marker = 'x')#, label = "Particle-Particle SLiding Friction = 1 \nParticle-Particle Rolling Friction = 1")
+
+# Creating line plot of means
+
+mean_s0_r0 = []
+std_s0_r0 = []
+
+mean_s1_r0 = []
+std_s1_r0 = []
+
+mean_s0_r1 = []
+std_s0_r1 = []
+
+mean_s1_r1 = []
+std_s1_r1 = []
+
+
+for i in repeats_s0_r0:
+    mean_s0_r0.append(np.mean(i))
+    std_s0_r0.append(np.std(i))
+
+for i in repeats_s1_r0:
+    mean_s1_r0.append(np.mean(i))
+    std_s1_r0.append(np.std(i))
+    
+for i in repeats_s0_r1:
+    mean_s0_r1.append(np.mean(i))
+    std_s0_r1.append(np.std(i))
+
+for i in repeats_s1_r1:
+    mean_s1_r1.append(np.mean(i))
+    std_s1_r1.append(np.std(i))
+    
+# Creating shade of +- 1 standard deviation either side of the mean
+
+top_line_s0_r0 = np.asarray(mean_s0_r0) + np.asarray(std_s0_r0)
+bottom_line_s0_r0 = np.asarray(mean_s0_r0) - np.asarray(std_s0_r0)
+
+top_line_s1_r0 = np.asarray(mean_s1_r0) + np.asarray(std_s1_r0)
+bottom_line_s1_r0 = np.asarray(mean_s1_r0) - np.asarray(std_s1_r0)
+
+top_line_s0_r1 = np.asarray(mean_s0_r1) + np.asarray(std_s0_r1)
+bottom_line_s0_r1 = np.asarray(mean_s0_r1) - np.asarray(std_s0_r1)
+
+top_line_s1_r1 = np.asarray(mean_s1_r1) + np.asarray(std_s1_r1)
+bottom_line_s1_r1 = np.asarray(mean_s1_r1) - np.asarray(std_s1_r1)
+
+p1, = plt.plot(df_sliding_pw, mean_s0_r0, color = colors[0], linewidth = 0.7)
+f1 = plt.fill_between(df_sliding_pw, top_line_s0_r0, bottom_line_s0_r0, color=colors[0], alpha=.2)#, label = '1 Standard Deviation')
+    
+p2, = plt.plot(df_sliding_pw, mean_s1_r0, color = colors[1], linewidth = 0.7)
+f2 = plt.fill_between(df_sliding_pw, top_line_s1_r0, bottom_line_s1_r0, color=colors[1], alpha=.2)#, label = '1 Standard Deviation')
+
+p3, = plt.plot(df_sliding_pw, mean_s0_r1, color = colors[2], linewidth = 0.7)
+f3 = plt.fill_between(df_sliding_pw, top_line_s0_r1, bottom_line_s0_r1, color=colors[2], alpha=.2)#, label = '1 Standard Deviation')
+
+p4, = plt.plot(df_sliding_pw, mean_s1_r1, color = colors[3], linewidth = 0.7)
+f4 = plt.fill_between(df_sliding_pw, top_line_s1_r1, bottom_line_s1_r1, color=colors[3], alpha=.2)#, label = '1 Standard Deviation')
+
+# Figure formatting
+
+plt.grid(which='major', color='k', linestyle='-', alpha = 0.5)
+plt.grid(which='minor', color='black', linestyle='-', alpha = 0.2)
+plt.minorticks_on()   
+plt.xlim([0,1])
+plt.ylim([0.55,0.67])
+plt.tick_params(axis = 'both', labelsize = 11.5)
+plt.xlabel("Particle-Wall Sliding Friction Coefficient (-)", fontsize = 11.5)
+plt.ylabel("Final Packing Density (-)", fontsize = 11.5)
+
+handles, labels = ax.get_legend_handles_labels()
+
+legend_points = [(p1, s1), f1, (p2, s2), f2, (p3, s3), f3, (p4, s4), f4]
+legend_labels = ["Particle-Particle: \nSliding Friction = 0 \nRolling Friction = 0",
+                    "1 Standard \nDeviation",
+                    "Particle-Particle \nSliding Friction = 1 \nRolling Friction = 0",
+                    "1 Standard \nDeviation",
+                    "Particle-Particle: \nSliding Friction = 0 \nRolling Friction = 1",                     
+                    "1 Standard \nDeviation",
+                    "Particle-Particle: \nSliding Friction = 1 \nRolling Friction = 1 ",
+                    "1 Standard \nDeviation"]
+
+plt.legend(legend_points, legend_labels,  loc="upper left", bbox_to_anchor =(1, 1), ncol = 1, numpoints=8, handler_map={tuple: HandlerTuple(ndivide=None)}, frameon = False, labelspacing=1.9, fontsize = 11.5)
+
+plt.savefig('method_2_procedure_1_sliding_graph', bbox_inches="tight")
