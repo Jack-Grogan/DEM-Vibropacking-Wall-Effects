67026d2a8e6041053b75c9476220df90ee9a83db
Jack Grogan <142009601+Jack-Grogan@users.noreply.github.com>
2024-01-17 12:34:28 +0000

    Add files via upload

---
+++ method_3_wall_extremes/restitution_graph_launch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --time=00:20:00
+#SBATCH --nodes=1
+#SBATCH --constraint=icelake
+#SBATCH --ntasks=1
+
+
+module purge; module load bluebear
+module load bear-apps/2022a
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load matplotlib/3.5.2-foss-2022a
+
+export VENV_DIR="${HOME}/virtual-environments"
+export VENV_PATH="${VENV_DIR}/my-virtual-env-${BB_CPU}"
+
+# Create a master venv directory if necessary
+mkdir -p ${VENV_DIR}
+
+# Check if virtual environment exists and create it if not
+if [[ ! -d ${VENV_PATH}  ]]; then
+    python -m venv --system-site-packages ${VENV_PATH}
+fi
+
+# Activate the virtual environment
+source ${VENV_PATH}/bin/activate
+
+# Execute your Python scripts
+python restitution_packing_graphs_batch.py
